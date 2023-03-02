71e7d9b60929bd4587d71ab21717eaba7ce76bd0
EC2 Default User <ec2-user@ip-172-31-12-223.ap-northeast-1.compute.internal>
2023-03-02 01:42:25 +0000

    getItemByGenre作成

---
+++ src/itemHandler.js
@@ -0,0 +1,44 @@
+import { DynamoDB } from 'aws-sdk'
+
+export async function getItemByGenre(event, context) {
+    const genre = event.queryStringParameters.genre
+    const documentClient = new DynamoDB.DocumentClient({
+        apiVersion: '2012-08-10',
+        region: 'ap-northeast-1'
+    })
+    const result = await documentClient.scan({
+        TableName: "items",
+        ExpressionAttributeValues: {
+            ':topic' : parseInt(genre)
+        },
+        FilterExpression: 'contains (categories, :topic)',
+    }).promise()
+    
+    return result.Items
+}
+
+export async function getItems(event, context) {
+    const documentClient = new DynamoDB.DocumentClient({
+        apiVersion: '2012-08-10',
+        region: 'ap-northeast-1'
+    })
+    const result = await documentClient.scan({
+        TableName: "items"
+    }).promise()
+    
+    return result.Items
+}
+
+export async function getItemById(event, context) {
+    const id = event.queryStringParameters.itemId
+    const documentClient = new DynamoDB.DocumentClient({
+        apiVersion: '2012-08-10',
+        region: 'ap-northeast-1'
+    })
+    const result = await documentClient.get({
+        TableName: "items",
+        Key: { "id": id }
+    }).promise()
+    
+    return result.Item
+}
