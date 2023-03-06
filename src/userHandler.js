1cccc1e8f6808c374ba8dfb1c9e98650e1427a46
EC2 Default User <ec2-user@ip-172-31-12-223.ap-northeast-1.compute.internal>
2023-03-06 00:20:52 +0000

    ログイン実装

--- src/userHandler.js
+++ src/userHandler.js
@@ -1,5 +1,24 @@
 import { DynamoDB } from 'aws-sdk'
 
+export async function login(event, context) {
+    const requestBody = JSON.parse(event.body)
+    const documentClient = new DynamoDB.DocumentClient({
+        apiVersion: '2012-08-10',
+        region: 'ap-northeast-1'
+    })
+    const result = await documentClient.query({
+        ExpressionAttributeValues: {
+            ':m': requestBody.mailAddress,
+            ':pw': requestBody.password
+        },
+        KeyConditionExpression: 'mailAddress = :m',
+        FilterExpression: 'contains (password, :pw)',
+        TableName: 'users'
+    }).promise()
+    
+    return result.Items
+}
+
 export async function getUserById(event, content) {
     const id = event.queryStringParameters.id
     const documentClient = new DynamoDB.DocumentClient({
